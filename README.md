fc6e3a51a71962100f5208c3f8c1d7decdda8837
Coili <coili@disroot.org>
2025-01-14 03:06:47 +0100

    Update enum.rs

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 # Enum.rs
+<hr />
+<h2>🤔 What is Enum.rs ?</h2>
+<div align="justify">
+Enum.rs is a DLL that can be used as part of a pentest and/or red team.
+This DLL retrieves information about Windows systems.
+For example :
+<ul>
+  <li>Information about the current user ;</li>
+  <li>Information about other users;</li>
+  <li>Basic system information (OS, architecture, build version)</li>
+  <li>Installed anti-virus potential;</li>
+  <li>List of installed Windows patches;</li>
+  <li>Potential services with an unquoted path;</li>
+  <li>Network interfaces (with IP address and netmask).</li>
+</ul>
+</div>
 
-## Screenshot using Sliver (tool WIP)
-<img src="images/screenshot.jpg" alt="screenshot" />
+<h2>🛠️ Installation steps</h2>
+
+<p>1. Clone this repository</p>
+
+```
+git clone https://github.com/coili/enum.rs.git
+```
+
+<br />
+<p>2. Build the DLL</p>
+
+```
+cargo build --release
+```
+
+To cross-compile, you can use [cross-rs](https://github.com/cross-rs/cross) and Docker.
+
+```
+cross build --target x86_64-pc-windows-gnu --release
+```
+
+The DLL will be compiled in the target/release folder (or target/x86_64-pc-windows-gnu/release).
+
+
+<h2>📸 Screenshot using Sliver C2 (tool still WIP)</h2>
+<img src="images/enumrs_with_sliver.jpg" alt="enum.rs with sliver" />
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-#![no_mangle]
-
 use winapi::um::winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
 
+use colorized::*;
+use network_interface::NetworkInterface;
+use network_interface::NetworkInterfaceConfig;
 use std::collections::HashMap;
 use whoami::{lang, realname, username};
-use wmi::{COMLibrary, Variant, WMIConnection, WMIDateTime};
+use wmi::{COMLibrary, Variant, WMIConnection};
 
 #[no_mangle]
 pub extern "system" fn DllMain(
@@ -23,51 +24,26 @@ pub extern "system" fn DllMain(
 }
 
 pub fn main() {
-    user_enum();
+    let _ = user_enum();
     let _ = system_enum();
-    //network_enum();
+    let _ = service_enum();
+    network_enum();
 }
 
-pub fn user_enum() {
-    println!("\n[*] Current user informations:");
+pub fn user_enum() -> Result<(), Box<dyn std::error::Error>> {
+    println!("\n{}", "[*] Current user:".color(Colors::BrightCyanFg));
     println!("Username: {}", username());
     println!("Real name: {}", realname());
     println!("Language: {:?}", lang().collect::<Vec<String>>());
-}
 
-pub fn system_enum() -> Result<(), Box<dyn std::error::Error>> {
-    println!("\n[*] System informations:");
-    let wmi_con = WMIConnection::with_namespace_path("ROOT\\CIMv2", COMLibrary::new()?)?;
-    let os_results: Vec<HashMap<String, Variant>> = wmi_con.raw_query(
-        "SELECT CSName, Caption, BuildNumber, OSArchitecture FROM Win32_OperatingSystem",
-    )?;
-    for os_result in os_results {
-        if let Some(csname) = os_result.get("CSName") {
-            if let Variant::String(ref cs_name) = csname {
-                println!("Computer Name: {cs_name}");
-            }
-        }
-        if let Some(os_caption) = os_result.get("Caption") {
-            if let Variant::String(ref caption) = os_caption {
-                println!("OS: {caption}");
-            }
-        }
-        if let Some(os_build) = os_result.get("BuildNumber") {
-            if let Variant::String(ref build) = os_build {
-                println!("Build: {build}");
-            }
-        }
-        if let Some(os_arch) = os_result.get("OSArchitecture") {
-            if let Variant::String(ref arch) = os_arch {
-                println!("OS Architecture: {arch}");
-            }
-        }
-    }
-
-    println!("\n[*] Users informations:");
+    println!("\n{}", "[*] Other users:".color(Colors::BrightCyanFg));
     println!(
-        "{:<25} {:<35} {:<20} {:<35}\n",
+        "{:<25} {:<35} {:<20} {:<35}",
         "Username", "Full username", "Domain", "SID"
+    );
+    println!(
+        "{:<25} {:<35} {:<20} {:<35}",
+        "--------", "-------------", "------", "---"
     );
     let wmi_con = WMIConnection::with_namespace_path("ROOT\\CIMv2", COMLibrary::new()?)?;
     let users_results: Vec<HashMap<String, Variant>> =
@@ -110,18 +86,129 @@ pub fn system_enum() -> Result<(), Box<dyn std::error::Error>> {
         );
     }
 
-    println!("\n[*] AV Enumeration:");
-    let wmi_con = WMIConnection::with_namespace_path("ROOT\\SecurityCenter2", COMLibrary::new()?)?;
+    Ok(())
+}
 
+pub fn system_enum() -> Result<(), Box<dyn std::error::Error>> {
+    println!("\n{}", "[*] System:".color(Colors::BrightCyanFg));
+    let wmi_con = WMIConnection::with_namespace_path("ROOT\\CIMv2", COMLibrary::new()?)?;
+    let os_results: Vec<HashMap<String, Variant>> = wmi_con.raw_query(
+        "SELECT CSName, Caption, BuildNumber, OSArchitecture FROM Win32_OperatingSystem",
+    )?;
+    for os_result in os_results {
+        if let Some(csname) = os_result.get("CSName") {
+            if let Variant::String(ref cs_name) = csname {
+                println!("Computer Name: {cs_name}");
+            }
+        }
+        if let Some(os_caption) = os_result.get("Caption") {
+            if let Variant::String(ref caption) = os_caption {
+                println!("OS: {caption}");
+            }
+        }
+        if let Some(os_build) = os_result.get("BuildNumber") {
+            if let Variant::String(ref build) = os_build {
+                println!("Build: {build}");
+            }
+        }
+        if let Some(os_arch) = os_result.get("OSArchitecture") {
+            if let Variant::String(ref arch) = os_arch {
+                println!("OS Architecture: {arch}");
+            }
+        }
+    }
+
+    println!("\n{}", "[*] AV Enumeration:".color(Colors::BrightCyanFg));
+    let wmi_con = WMIConnection::with_namespace_path("ROOT\\SecurityCenter2", COMLibrary::new()?)?;
     let av_results: Vec<HashMap<String, Variant>> =
         wmi_con.raw_query("SELECT displayName FROM AntiVirusProduct")?;
     for av_result in av_results {
         if let Some(display_name) = av_result.get("displayName") {
             if let Variant::String(ref name) = display_name {
-                println!("{}", name);
+                println!("{}", name.color(Colors::BrightRedFg));
             }
         } else {
             println!("No av found!")
+        }
+    }
+
+    println!("\n{}", "[*] Windows patches".color(Colors::BrightCyanFg));
+    println!("{:<20} {:<15} {:<20}", "Computer", "HotFixID", "Type");
+    println!("{:<20} {:<15} {:<20}", "--------", "--------", "----");
+    let wmi_con = WMIConnection::with_namespace_path("ROOT\\CIMv2", COMLibrary::new()?)?;
+    let patches_results: Vec<HashMap<String, Variant>> =
+        wmi_con.raw_query("SELECT CSName, HotFixID, Description FROM Win32_QuickFixEngineering")?;
+
+    let mut print_hotcsname: String;
+    let mut print_hotfixid: String;
+    let mut print_hotdesc: String;
+    for patches_result in patches_results {
+        print_hotcsname = String::from("");
+        print_hotfixid = String::from("");
+        print_hotdesc = String::from("");
+
+        if let Some(hotfix_csname) = patches_result.get("CSName") {
+            if let Variant::String(ref csname) = hotfix_csname {
+                print_hotcsname.push_str(csname);
+            }
+        }
+
+        if let Some(hotfix_hotfixid) = patches_result.get("HotFixID") {
+            if let Variant::String(ref hotfixid) = hotfix_hotfixid {
+                print_hotfixid.push_str(hotfixid);
+            }
+        }
+
+        if let Some(hotfix_desc) = patches_result.get("Description") {
+            if let Variant::String(ref desc) = hotfix_desc {
+                print_hotdesc.push_str(desc);
+            }
+        }
+
+        println!(
+            "{:<20} {:<15} {:<20}",
+            print_hotcsname, print_hotfixid, print_hotdesc
+        );
+    }
+
+    Ok(())
+}
+
+pub fn service_enum() -> Result<(), Box<dyn std::error::Error>> {
+    println!("\n{}", "[*] Current services:".color(Colors::BrightCyanFg));
+    println!("-> Checking unquoted path:");
+    println!("\t{:<35} {}", "Service name", "Service path");
+    println!("\t{:<35} {}", "------------", "------------");
+    let wmi_con = WMIConnection::with_namespace_path("ROOT\\CIMv2", COMLibrary::new()?)?;
+    let services_results: Vec<HashMap<String, Variant>> =
+        wmi_con.raw_query("SELECT Name, PathName FROM Win32_Service")?;
+
+    let mut print_scname: String;
+    let mut print_scpath: String;
+    for services_result in services_results {
+        print_scname = String::from("");
+        print_scpath = String::from("");
+
+        if let Some(sc_name) = services_result.get("Name") {
+            if let Variant::String(ref name) = sc_name {
+                print_scname.push_str(name);
+            }
+        }
+
+        if let Some(sc_path) = services_result.get("PathName") {
+            if let Variant::String(ref path) = sc_path {
+                print_scpath.push_str(path);
+            }
+        }
+
+        if print_scpath.contains(" ") {
+            if !print_scpath.to_lowercase().contains("system32") {
+                println!(
+                    "\t{:<35} {}",
+                    print_scname,
+                    print_scpath.color(Colors::BrightYellowFg)
+                );
+            }
         }
     }
 
@@ -129,6 +216,39 @@ pub fn system_enum() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 pub fn network_enum() {
-    println!("\n[*] Network interfaces:");
-    // TO DO
+    println!(
+        "\n{}",
+        "[*] Network interfaces:".color(Colors::BrightCyanFg)
+    );
+    println!(
+        "{:<15} {:<15} {:<15}",
+        "Interface name", "IP address", "Netmask"
+    );
+    println!(
+        "{:<15} {:<15} {:<15}",
+        "--------------", "----------", "-------"
+    );
+    let network_interfaces = NetworkInterface::show().unwrap();
+
+    let mut print_interface: String;
+    let mut print_ip: String;
+    let mut print_netmask: String;
+    for interface in network_interfaces.iter() {
+        print_interface = interface.name.to_owned();
+        print_ip = interface.addr.get(1).unwrap().ip().to_string();
+        print_netmask = interface
+            .addr
+            .get(1)
+            .unwrap()
+            .netmask()
+            .unwrap()
+            .to_string();
+
+        if !print_interface.to_lowercase().contains("loopback") {
+            println!(
+                "{:<15} {:<15} {:<15}",
+                print_interface, print_ip, print_netmask
+            );
+        }
+    }
 }
